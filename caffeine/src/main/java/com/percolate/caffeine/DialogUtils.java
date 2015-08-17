33999ac5f6dd9bd74d82e8566952c8cbe4e6af14
Joshua Williams <joshua.williams@percolate.com>
2015-08-17 15:52:26 -0400

    Added null check for dialog in first test

--- caffeine/src/main/java/com/percolate/caffeine/DialogUtils.java
+++ caffeine/src/main/java/com/percolate/caffeine/DialogUtils.java
@@ -23,19 +23,20 @@ public class DialogUtils {
     /**
      * Show a model dialog box.  The <code>android.app.AlertDialog</code> object is returned so that
      * you can specify an OnDismissListener (or other listeners) if required.
-     * If an AlertDialog created by this class is already being shown, the AlertDialog will only be
-     * created and returned.
+     * If an AlertDialog created by this class is already being shown, it will return null.
      * <b>Note:</b> show() is already called on the AlertDialog being returned.
      *
      * @param context The current Context or Activity that this method is called from.
      * @param message Message to display in the dialog.
      * @return AlertDialog that is being displayed.
      */
-    public static AlertDialog quickDialog(final Activity context, final String message, boolean singleDialog) {
+    public static AlertDialog quickDialog(final Activity context, final String message,
+                                          boolean singleDialog) {
         final SpannableString s = new SpannableString(message); //Make links clickable
         Linkify.addLinks(s, Linkify.ALL);
         if (singleDialog) {
-            if (activityName != null && !activityName.equals(context.getLocalClassName())) {
+            if (activityName != null && !activityName.equals(context.getLocalClassName())
+                    && dialog != null) {
                 dialog.dismiss();
             }
 
@@ -51,10 +52,12 @@ public class DialogUtils {
     }
 
     /**
-     * Creates an <code>android.app.AlertDialog</code> object with the message in the parameters and returns it.
+     * Creates an <code>android.app.AlertDialog</code> object with the message in the parameters
+     * and returns it.
      *
      * @param context The current Context or Activity that this method is called from.
-     * @param s Message to display in the dialog that has been modified to a <code>	android.text.SpannableString</code> object.
+     * @param s Message to display in the dialog that has been modified to a
+     *          <code>android.text.SpannableString</code> object.
      * @return AlertDialog to be displayed.
      */
     private static AlertDialog createAlertDialog(Activity context, SpannableString s){
@@ -63,7 +66,8 @@ public class DialogUtils {
         builder.setPositiveButton(android.R.string.ok, closeDialogListener());
         AlertDialog dialog = builder.create();
         dialog.show();
-        ((TextView) dialog.findViewById(android.R.id.message)).setMovementMethod(LinkMovementMethod.getInstance()); //Make links clickable
+        ((TextView) dialog.findViewById(android.R.id.message))
+                .setMovementMethod(LinkMovementMethod.getInstance()); //Make links clickable
         return dialog;
     }
 
